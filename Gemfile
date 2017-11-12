ad26d1db6f2d45e3cce5d277deb973bcbb79da13
Jenkcho <Jenkcho@gmail.com>
2017-11-12 08:01:58 +0000

    added devise gem

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'spring',         group: :development
-
+gem 'devise', '~> 4.3'
 
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Added as per Devise installation
+  config.action_mailer.default_url_options = { host: 'jenkcho-project.herokuapp.com'}
 end
